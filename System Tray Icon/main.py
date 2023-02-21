c1892e4c7447c1805d5f7c03a1fedddad19de5a1
Jordan Brunner <89763188+JABrunner@users.noreply.github.com>
2023-02-20 20:26:02 -0600

    Update main.py
    Fixed bug that prevented app from starting

--- System Tray Icon/main.py
+++ System Tray Icon/main.py
@@ -1,29 +1,22 @@
 import pystray
 import PIL.Image
 
-#load image
-image = PIL.Image.open("circuit_96px.png")
+image = PIL.Image.open("System-Tray-Icon-main/System Tray Icon/circuit_96px.png")
 
-#define events for items clicked
 def on_clicked(icon, item):
-    if str(item) == "Say Hello to My Little Friend":
-        print("Hello World")
+    if str(item) == "Say Hello":
+        print("Greetings")
     elif str(item) == "Exit":
-        #close app on this selection
-            icon.stop()
-    elif str(item) == "This doesn't actually do anything, Don't Click":
-        print("THIS DOESN'T DO ANYTHING!!!!")
-    else:
-        print("Not Implemented yet")
+        icon.stop()
+    elif str(item) == "Hey Look a Submenu":
+        print("Nothing implemented yet")
 
-#set menu list items
-icon = pystray.Icon("System Tray Mockup", image, menu=pystray.Menu(
-    pystray.MenuItem("Say Hello to My Little Friend", on_clicked),
-    pystray.MenuItem("Exit", on_clicked),
-    pystray.Menu("Theres More?!?!?", pystray.Menu(
-        pystray.Menu("This doesn't actually do anything, Don't Click")
-    ))
+icon = pystray.Icon("Test", image, menu=pystray.Menu(
+    pystray.MenuItem("Say Hello", on_clicked),
+    pystray.MenuItem("Hey Look a Submenu", on_clicked),
+    pystray.MenuItem("Exit", on_clicked)
+
+    
 ))
-
 
 icon.run()
